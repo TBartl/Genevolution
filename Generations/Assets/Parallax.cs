c385e63623141a295fab31f9f42ac1e921b818e3
TBartl <tbartl@umich.edu>
2018-01-27 23:25:17 -0500

    More tiles, animations

---
+++ Generations/Assets/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+	public float amount;
+
+	Transform camTransform;
+
+	Vector3 originalPos;
+	Vector3 originalCamPos;
+
+	// Use this for initialization
+	void Start () {
+		originalPos = this.transform.position;
+
+		camTransform = Camera.main.transform;
+		originalCamPos = camTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 offset = camTransform.position - originalCamPos;
+		this.transform.position = originalPos + offset * amount;		
+	}
+}
